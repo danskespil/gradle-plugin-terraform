3eebf7ccbfa88fc2bb9a5c62ee1ecbc223ddfda6
Jesper Wermuth <wermuth@lundogbendsen.dk>
2017-10-03 11:51:06 +0200

    Apply can read files provided by plan

--- src/main/groovy/dk/danskespil/gradle/plugins/terraform/Apply.groovy
+++ src/main/groovy/dk/danskespil/gradle/plugins/terraform/Apply.groovy
@@ -1,11 +1,34 @@
 package dk.danskespil.gradle.plugins.terraform
 
+import dk.danskespil.gradle.plugins.helpers.dscommandlineexecutor.DSCommandLineExecutorFactory
+import org.gradle.api.tasks.InputFile
+import org.gradle.api.tasks.Optional
+import org.gradle.api.tasks.TaskAction
+import org.gradle.process.ExecSpec
+
 /**
  * Wraps cli command: terraform apply
  */
 class Apply extends TerraformTask {
+    @Optional
+    @InputFile
+    File plan
+
     @Override
     String getDescription() {
         return """Wraps cli command: terraform apply"""
+    }
+
+    @TaskAction
+    action() {
+        commandLine.addToEnd('terraform', 'apply')
+
+        if (plan) {
+            commandLine.addToEnd(plan.name)
+        }
+
+        DSCommandLineExecutorFactory.createExecutor(project).executeExecSpec(this, { ExecSpec e ->
+            e.commandLine this.commandLine
+        })
     }
 }
--- src/test/groovy/dk/danskespil/gradle/plugins/terraform/ApplyTest.groovy
+++ src/test/groovy/dk/danskespil/gradle/plugins/terraform/ApplyTest.groovy
@@ -3,5 +3,55 @@ package dk.danskespil.gradle.plugins.terraform
 import dk.danskespil.gradle.plugins.helpers.DSSpecification
 
 class ApplyTest extends DSSpecification {
+    def "When calling custom terraform apply task, the executed commandline looks as expected"() {
+        given:
+        buildFile << """
+          plugins {
+              id 'dk.danskespil.gradle.plugins.terraform'
+          }
+  
+          task cut(type: dk.danskespil.gradle.plugins.terraform.Apply)
+        """
 
+        when:
+        def build = buildWithTasks(':cut')
+
+        then:
+        build.output.contains('terraform apply')
+    }
+
+    def "When calling the terraform apply task that is provided by the plugin, the executed commandline looks as expected"() {
+        given:
+        buildFile << """
+          plugins {
+              id 'dk.danskespil.gradle.plugins.terraform'
+          }
+        """
+
+        when:
+        def build = buildWithTasks(':tfApply')
+
+        then:
+        build.output.contains('terraform apply')
+    }
+
+    def "When calling custom terraform apply task, you can configure which plan file you want to read from "() {
+        given:
+        buildFile << """
+          plugins {
+            id 'dk.danskespil.gradle.plugins.terraform'
+
+          }
+          task cut(type: dk.danskespil.gradle.plugins.terraform.Apply) {
+            plan = file('plan.bin')
+          }
+        """
+        createNewPath('plan.bin') << "binary-content"
+
+        when:
+        def build = buildWithTasks(':cut')
+
+        then:
+        build.output.contains('terraform apply plan.bin')
+    }
 }

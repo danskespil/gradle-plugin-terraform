fbf62e7e038bc20e193dd45cbe309716cad57e01
Jesper Wermuth <wermuth@lundogbendsen.dk>
2017-10-04 15:06:47 +0200

    Rebuild when modules change only

--- src/main/groovy/dk/danskespil/gradle/plugins/terraform/Get.groovy
+++ src/main/groovy/dk/danskespil/gradle/plugins/terraform/Get.groovy
@@ -2,13 +2,16 @@ package dk.danskespil.gradle.plugins.terraform
 
 import dk.danskespil.gradle.plugins.helpers.dscommandlineexecutor.DSCommandLineExecutorFactory
 import org.gradle.api.file.FileCollection
+import org.gradle.api.tasks.InputFiles
 import org.gradle.api.tasks.OutputFiles
 import org.gradle.api.tasks.TaskAction
 import org.gradle.process.ExecSpec
 
 class Get extends TerraformBaseTask {
     @OutputFiles
-    FileCollection outputFilesSoGradleOnlyBuildsWhenItChanges = project.fileTree('.terraform/modules')
+    FileCollection oOutputFilesSoGradleOnlyBuildsWhenItChanges = project.fileTree('.terraform/modules')
+    @InputFiles
+    FileCollection iOutputFilesSoGradleOnlyBuildsWhenItChanges = project.fileTree('.terraform/modules')
 
     @TaskAction
     action() {

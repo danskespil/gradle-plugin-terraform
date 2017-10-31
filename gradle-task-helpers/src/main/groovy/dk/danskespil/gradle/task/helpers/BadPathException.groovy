30e3174ba4b08681ca3611b9e74315ff0cd6838b
Jesper Wermuth <jwermuth@gmail.com>
2017-10-31 22:45:29 +0100

    replaced local project 'gradle-task-helpers' with shared version

--- gradle-task-helpers/src/main/groovy/dk/danskespil/gradle/task/helpers/BadPathException.groovy
+++
@@ -1,7 +0,0 @@
-package dk.danskespil.gradle.task.helpers
-
-class BadPathException extends RuntimeException{
-    BadPathException(String badInput) {
-        super("You are trying to parse '#{badInput}' as a path. Hm. Does not work. Try something like 'path/to/something'")
-    }
-}
